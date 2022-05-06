87793867a6581c3a8d82c571c7356b58e08b5a6b
Thomas Månsson <ThomasCreate@thomascreate.mynet>
2022-05-06 11:57:19 +0200

    First commit

---
+++ EggTimerStartFiler/EggTimer/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  EggTimer
+//
+//  Created by Thomas Månsson on 04/02/2022.
+//  Copyright © 2022 ThomasCreate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    
+}
+
